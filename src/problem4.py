80898e796692412f7adeff4473f5c1b9b0867028
novotnaj <novotnaj@rose-hulman.edu>
2018-05-24 14:03:01 -0400

    Finished 4

--- src/problem4.py
+++ src/problem4.py
@@ -2,13 +2,13 @@
 Final exam, problem 4.
 
 Authors: David Mutchler, Dave Fisher, Matt Boutell, Amanda Stouder,
-         their colleagues and PUT_YOUR_NAME_HERE.  May 2018.
+         their colleagues and Andrew Novotny.  May 2018.
 
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+"""  # DONE: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 
 ###############################################################################
-# TODO: 2.
+# DONE: 2.
 #   In this problem, you will go through the methods of the  Pig  class
 #   that is defined below, one by one, in the order that they appear.
 #   For each method:
@@ -28,6 +28,22 @@ def main():
     #     -- Construct two Pig objects
     #     -- Call each method that you implement below.
     # -------------------------------------------------------------------------
+    oinker = Pig(200)
+    print("Oinker's weight is", oinker.get_weight())
+    oinker.eat(50)
+    print("Oinker's weight after eating 50 lbs slop", oinker.get_weight())
+    oinker.eat_for_a_year()
+    print("Oinker's weight after eating for a year", oinker.get_weight())
+    oinker2 = Pig(100)
+    oinker3 = Pig(200)
+    print("Oinker2's weight:", oinker2.get_weight())
+    print("Oinker3's weight:", oinker3.get_weight())
+    print("Should show Oinker3's weight:",oinker2.heavier_pig(oinker3).get_weight())
+    print("Should show Oinker3's weight:", oinker3.heavier_pig(oinker2).get_weight())
+    oinker4 = oinker2.new_pig(oinker)
+    print("Oinker 4 should have the same weight as Oinker")
+    print("Oinker's weight:", oinker.get_weight())
+    print("Oinker4's weight:", oinker4.get_weight())
 
 
 class Pig(object):
@@ -36,17 +52,23 @@ class Pig(object):
         What comes in:  The Pig's weight (in pounds).
         Side effects: Sets instance variables as needed by the other methods.
         """
-        # TODO: Implement and test this method.
+        # DONE: Implement and test this method.
+        self.weight = weight
 
     def get_weight(self):
         """ Returns this Pig's weight. """
-        # TODO: Implement and test this method.
+        # DONE: Implement and test this method.
+        return self.weight
 
     def eat(self, pounds_of_slop):
         """
         Increments this Pig's weight by the given pounds_of_slop.
         """
-        # TODO: Implement and test this method.
+        # DONE: Implement and test this method.
+
+        #### I think this ratio is a little extreme, but I don't think it makes much of a difference
+        #### The premise to the problem should be the same
+        self.weight += pounds_of_slop
 
     def eat_for_a_year(self):
         """
@@ -58,22 +80,28 @@ class Pig(object):
           -- eat 364 pounds of slop, then
           -- eat 365 pounds of slop.
         """
-        # TODO: Implement and test this method.
+        # DONE: Implement and test this method.
+        for k in range(365):
+            self.eat(k+1)
 
     def heavier_pig(self, other_pig):
         """
         Returns either this Pig object or the other given Pig object,
         whichever is heavier.
         """
-        # TODO: Implement and test this method.
+        # DONE: Implement and test this method.
+        if self.get_weight() > other_pig.get_weight():
+            return self
+        else:
+            return other_pig
 
     def new_pig(self, other_pig):
         """
         Returns a new Pig whose weight is the weight of the heavier
           of this Pig and the other_Pig.
         """
-        # TODO: Implement and test this method.
-
+        # DONE: Implement and test this method.
+        return Pig(self.heavier_pig(other_pig).get_weight())
 
 # -----------------------------------------------------------------------------
 # Calls  main  to start the ball rolling.
