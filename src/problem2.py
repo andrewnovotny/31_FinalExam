4758446e19458afacb19ab8b4b0cca6f07dca2dc
novotnaj <novotnaj@rose-hulman.edu>
2018-05-24 13:30:38 -0400

    Finished 2

--- src/problem2.py
+++ src/problem2.py
@@ -2,9 +2,9 @@
 Final exam, problem 2.
 
 Authors: David Mutchler, Dave Fisher, Matt Boutell, Amanda Stouder,
-         their colleagues and PUT_YOUR_NAME_HERE.  May 2018.
+         their colleagues and Andrew Novotny.  May 2018.
 
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+"""  # DONE: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 import time
 import testing_helper
@@ -19,9 +19,9 @@ def main():
     print()
     print('Un-comment the calls in MAIN one by one')
     print(' to run the testing code as you complete the TODOs.')
-    # run_test_problem2a()
-    # run_test_problem2b()
-    # run_test_problem2c()
+    run_test_problem2a()
+    run_test_problem2b()
+    run_test_problem2c()
 
 
 def is_prime(n):
@@ -170,10 +170,14 @@ def problem2a(x, sequence):
       :type sequence:  [int]
     """
     # -------------------------------------------------------------------------
-    # TODO: 2. Implement and test this function.
+    # DONE: 2. Implement and test this function.
     #          Tests have been written for you (above).
     # -------------------------------------------------------------------------
-
+    newlist = []
+    for k in range(len(sequence)):
+        if sequence[k] > x:
+            newlist.append(sequence[k])
+    return newlist
 
 def run_test_problem2b():
     """ Tests the  problem2b   function. """
@@ -250,10 +254,14 @@ def problem2b(sequence):
       :type sequence  [int]
     """
     # -------------------------------------------------------------------------
-    # TODO: 3. Implement and test this function.
+    # DONE: 3. Implement and test this function.
     #          Tests have been written for you (above).
     # -------------------------------------------------------------------------
-
+    largest = 0
+    for k in range(len(sequence)):
+        if abs(sequence[k]) > abs(sequence[largest]):
+            largest = k
+    return largest
 
 def run_test_problem2c():
     """ Tests the  problem2c   function. """
@@ -393,10 +401,14 @@ def problem2c(x):
       :type x:    int
     """
     # -------------------------------------------------------------------------
-    # TODO: 4. Implement and test this function.
+    # DONE: 4. Implement and test this function.
     #          Tests have been written for you (above).
     # -------------------------------------------------------------------------
-
+    while True:
+        if is_prime(x):
+            if is_prime(sum_of_digits(x)):
+                return x
+        x += 1
 
 ###############################################################################
 # Our tests use the following to print error messages in red.
