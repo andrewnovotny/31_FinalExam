08821d9525b0a52296491ef738c888ae95b3b395
novotnaj <novotnaj@rose-hulman.edu>
2018-05-24 14:08:30 -0400

    Finished 4

--- src/problem5.py
+++ src/problem5.py
@@ -2,9 +2,9 @@
 Final exam, problem 5.
 
 Authors: David Mutchler, Dave Fisher, Matt Boutell, Amanda Stouder,
-         their colleagues and PUT_YOUR_NAME_HERE.  May 2018.
+         their colleagues and Andrew Novotny.  May 2018.
 
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+"""  # DONE: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 import time
 import testing_helper
@@ -136,10 +136,15 @@ def problem5(m, numbers1, numbers2):
       :type [str]
     """
     # -------------------------------------------------------------------------
-    # TODO: 3. Implement and test this function.
+    # DONE: 3. Implement and test this function.
     #          Tests have been written for you (above).
     # -------------------------------------------------------------------------
-
+    newlist = []
+    for k in range(len(numbers1)):
+        if numbers1[k] * m == numbers2[k]:
+            newlist.append(numbers1[k])
+            newlist.append(numbers2[k])
+    return newlist
 
 ###############################################################################
 # Our tests use the following to print error messages in red.
